756e5f0b2ce7481a13338b5b02c550e6542e92ac
Rodrigo Silva <47982767+rodsil01@users.noreply.github.com>
2022-09-03 00:16:12 -0500

    style: menu option min width

--- src/components/common/menu/MenuOptions.tsx
+++ src/components/common/menu/MenuOptions.tsx
@@ -18,7 +18,7 @@ const MenuOptions = ({ options }: MenuOptionsProps) => {
             </button>
             {
                 isMenuOpen &&
-                <div className="relative -right-3 top-3 bg-surface border rounded-md shadow-md z-20">
+                <div className="relative -right-3 top-3 bg-surface border rounded-md shadow-md z-20 min-w-[150px]">
                     <ul>
                         {
                             options.map(
