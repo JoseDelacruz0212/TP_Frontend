c6e03f8d939d0cb25637252c7fb92172fe51b3d3
Rodrigo Silva <47982767+rodsil01@users.noreply.github.com>
2022-09-17 21:41:54 -0500

    refactor: add useSelect hook

--- src/containers/selects/CoursesSelect.tsx
+++ src/containers/selects/CoursesSelect.tsx
@@ -1,59 +1,33 @@
-import React, {useEffect, useState} from "react";
-import Select, {PropsValue} from "react-select";
+import React from "react";
+import Select from "react-select";
 
 import {CourseOption} from "../../types/communication/responses/courses";
 
-import {SelectOption, selectStyles} from "../../components/common/select/Select";
-
 import CourseService from "../../services/CourseService";
+import useSelect from "../../hooks/useSelect";
+
+const getValue = (x: CourseOption) => x.id;
+const getLabel = (x: CourseOption) => x.name;
 
 const CourseSelect = ({ courseId, onCourseChanged }: { courseId?: string, onCourseChanged: (x?: string) => void }) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [courses, setCourses] = useState<CourseOption[]>([]);
-    const [selectedOption, setSelectedOption] = useState<PropsValue<SelectOption>>(null);
-
-    const onSelectedCourseIdHandler = (courseId?: string) => {
-        onCourseChanged(courseId);
-
-        const selectedCourse = courses.find(x => x.id === courseId);
-        if (selectedCourse) {
-            const newSelectedOption = { value: selectedCourse.id, label: selectedCourse.name };
-            setSelectedOption(newSelectedOption);
-        }
-    }
-
-    useEffect(() => {
-        setIsLoading(true);
-
-        CourseService.getCoursesForCombo().then(
-            values => {
-                setIsLoading(false);
-                setCourses(values);
-
-                const selectedCourse = values.find(x => x.id === courseId);
-                if (selectedCourse) {
-                    const newSelectedOption = { value: selectedCourse.id, label: selectedCourse.name };
-                    setSelectedOption(newSelectedOption);
-                }
-            }
-        ).catch(() => setIsLoading(false));
-    }, []);
+    const selectProps = useSelect(
+        CourseService.getCoursesForCombo,
+        getValue,
+        getLabel,
+        onCourseChanged,
+        courseId
+    );
 
     return (
         <div className="form-group">
             <label htmlFor="edit-assessment-course" className="form-label">
                 <small>Curso</small>
             </label>
-            <Select id="edit-assessment-course"
+            <Select {...selectProps}
+                    id="edit-assessment-course"
                     name="edit-assessment-course"
                     placeholder="Curso"
-                    isDisabled={!courses || courses?.length === 0}
-                    styles={selectStyles}
-                    value={selectedOption}
-                    isLoading={isLoading}
-                    noOptionsMessage={({ inputValue }) => "No se encontraron cursos"}
-                    onChange={(option: SelectOption | null) => onSelectedCourseIdHandler(option?.value || undefined)}
-                    options={courses.map(course => ({ value: course.id, label: course.name }))} />
+                    noOptionsMessage={({ inputValue }) => "No se encontraron cursos"} />
         </div>
     )
 };
--- src/containers/selects/InstitutionsSelect.tsx
+++ src/containers/selects/InstitutionsSelect.tsx
@@ -1,59 +1,33 @@
-import React, {useEffect, useState} from "react";
-import Select, {PropsValue} from "react-select";
+import React from "react";
+import Select from "react-select";
 
 import {InstitutionOption} from "../../types/communication/responses/institutions";
 
-import {SelectOption, selectStyles} from "../../components/common/select/Select";
-
 import InstitutionService from "../../services/InstitutionService";
+import useSelect from "../../hooks/useSelect";
+
+const getValue = (x: InstitutionOption) => x.id;
+const getLabel = (x: InstitutionOption) => x.name;
 
 const InstitutionsSelect = ({ institutionId, onInstitutionChanged }: { institutionId?: string, onInstitutionChanged: (x?: string) => void }) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [institutions, setInstitutions] = useState<InstitutionOption[]>([]);
-    const [selectedOption, setSelectedOption] = useState<PropsValue<SelectOption>>(null);
-
-    const onSelectedInstitutionIdHandler = (institutionId?: string) => {
-        onInstitutionChanged(institutionId);
-
-        const selectedInstitution = institutions.find(x => x.id === institutionId);
-        if (selectedInstitution) {
-            const newSelectedOption = { value: selectedInstitution.id, label: selectedInstitution.name };
-            setSelectedOption(newSelectedOption);
-        }
-    }
-
-    useEffect(() => {
-        setIsLoading(true);
-
-        InstitutionService.getInstitutionsForCombo().then(
-            values => {
-                setIsLoading(false);
-                setInstitutions(values);
-
-                const selectedInstitution = values.find(x => x.id === institutionId);
-                if (selectedInstitution) {
-                    const newSelectedOption = { value: selectedInstitution.id, label: selectedInstitution.name };
-                    setSelectedOption(newSelectedOption);
-                }
-            }
-        ).catch(() => setIsLoading(false));
-    }, []);
+    const selectProps = useSelect(
+        InstitutionService.getInstitutionsForCombo,
+        getValue,
+        getLabel,
+        onInstitutionChanged,
+        institutionId
+    );
 
     return (
         <div className="form-group">
             <label htmlFor="edit-course-institution" className="form-label">
                 <small>Institución</small>
             </label>
-            <Select id="edit-course-institution"
+            <Select {...selectProps}
+                    id="edit-course-institution"
                     name="edit-course-institution"
                     placeholder="Institución"
-                    isDisabled={!institutions || institutions?.length === 0}
-                    styles={selectStyles}
-                    value={selectedOption}
-                    isLoading={isLoading}
-                    noOptionsMessage={({ inputValue }) => "No se encontraron instituciones"}
-                    onChange={(option: SelectOption | null) => onSelectedInstitutionIdHandler(option?.value || "")}
-                    options={institutions.map(institution => ({ value: institution.id, label: institution.name }))} />
+                    noOptionsMessage={({ inputValue }) => "No se encontraron instituciones"} />
         </div>
     )
 };
