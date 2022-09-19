2a79f3d0124e637b99d0f41b3df9231453fb5786
Rodrigo Silva <47982767+rodsil01@users.noreply.github.com>
2022-09-18 20:05:25 -0500

    fix: add storage encryption

--- src/config/httpClients/authorizationClient.ts
+++ src/config/httpClients/authorizationClient.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-import {API_BASE_URL} from "../app/basic-settings";
 
 const instance = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     timeout: 5000
 });
 
