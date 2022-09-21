337a6795b8604b71efdcd4a3179c9cdb33f53cbf
Rodrigo Silva <47982767+rodsil01@users.noreply.github.com>
2022-09-21 01:02:48 -0500

    fix: services

--- src/services/QualificationBlockchainService.ts
+++ src/services/QualificationBlockchainService.ts
@@ -1,6 +1,8 @@
+import { v4 as uuid } from 'uuid';
+
 import {FetchService} from "./FetchService";
 import {
-    mapToQualificationGroup,
+    mapToQualificationGroup, Qualification,
     QualificationGroup
 } from "../types/communication/responses/qualification";
 import {QualificationFilter} from "../types/communication/requests/qualification";
@@ -13,8 +15,8 @@ import {Assessment} from "../types/communication/responses/assessment";
 class QualificationBlockchainService extends FetchService<QualificationGroup, QualificationFilter> {
     getData(filters: QualificationFilter, page: number = 1, pageSize: number = 10): Promise<QualificationGroup[] | PaginatedResponse<QualificationGroup>> {
         if (filters.userId) {
-            return blockchainClient.get<any>(`/history/${filters.userId}`)
-                .then(({data: { response }}) => this.getPaginatedData(mapToQualificationGroup(response), filters, page, pageSize))
+            return blockchainClient.get<{ response: Qualification[] }>(`/history/${filters.userId}`)
+                .then(({data: { response }}) => this.getPaginatedData(mapToQualificationGroup(response.map(x => ({ ...x, id: uuid() }))), filters, page, pageSize))
                 .catch(() => Promise.reject("Ocurrió un error al tratar de obtener las calificaciones"));
         } else return new Promise(resolve => resolve([]));
     }
