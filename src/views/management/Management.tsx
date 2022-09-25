c5641cc5e7dcd3b222786676d21b837fc579cdff
Rodrigo Silva <47982767+rodsil01@users.noreply.github.com>
2022-09-25 10:49:34 -0500

    fix: change tag to <a/>

--- src/views/management/Management.tsx
+++ src/views/management/Management.tsx
@@ -3,12 +3,12 @@ import {Link} from "react-router-dom";
 import {IoArrowForwardOutline} from "react-icons/io5";
 
 const Management = () => (
-    <Link to={process.env.REACT_APP_ADMINISTRATION_PANEL as string}>
+    <a href={process.env.REACT_APP_ADMINISTRATION_PANEL as string}>
         <span className="text-primary-dark flex items-center space-x-1">
             <span>Ir a panel de administación de la red Blockchain</span>
             <span><IoArrowForwardOutline /></span>
         </span>
-    </Link>
+    </a>
 );
 
 export default Management;
