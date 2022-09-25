971e62199529a05f7684e63d51f8c171a5db5198
Rodrigo Silva <47982767+rodsil01@users.noreply.github.com>
2022-09-25 10:43:00 -0500

    fix: add string type

--- src/views/management/Management.tsx
+++ src/views/management/Management.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 import {IoArrowForwardOutline} from "react-icons/io5";
 
 const Management = () => (
-    <Link to={process.env.REACT_APP_ADMINISTRATION_PANEL}>
+    <Link to={process.env.REACT_APP_ADMINISTRATION_PANEL as string}>
         <span className="text-primary-dark flex items-center space-x-1">
             <span>Ir a panel de administación de la red Blockchain</span>
             <span><IoArrowForwardOutline /></span>
