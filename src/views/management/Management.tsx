57e1e0f38dc71d035c19d8f70de5955274eb8a29
Rodrigo Silva <47982767+rodsil01@users.noreply.github.com>
2022-09-25 10:17:42 -0500

    fix: add link to blockchain management site

--- src/views/management/Management.tsx
+++ src/views/management/Management.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {IoArrowForwardOutline} from "react-icons/io5";
 
 const Management = () => (
-    <iframe src="http://ec2-44-203-113-105.compute-1.amazonaws.com:8080/#/" className="w-full h-full" />
+    <Link to="http://ec2-44-203-113-105.compute-1.amazonaws.com:8080/#/">
+        <span className="text-primary-dark flex items-center space-x-1">
+            <span>Ir a panel de administación de la red Blockchain</span>
+            <span><IoArrowForwardOutline /></span>
+        </span>
+    </Link>
 );
 
 export default Management;
