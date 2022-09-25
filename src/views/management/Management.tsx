a14a07bd16be180e416e9f67fc0907930ae58f25
Rodrigo Silva <47982767+rodsil01@users.noreply.github.com>
2022-09-25 10:30:30 -0500

    task: add env variable

--- src/views/management/Management.tsx
+++ src/views/management/Management.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 import {IoArrowForwardOutline} from "react-icons/io5";
 
 const Management = () => (
-    <Link to="http://ec2-44-203-113-105.compute-1.amazonaws.com:8080/#/">
+    <Link to={process.env.REACT_APP_ADMINISTRATION_PANEL}>
         <span className="text-primary-dark flex items-center space-x-1">
             <span>Ir a panel de administación de la red Blockchain</span>
             <span><IoArrowForwardOutline /></span>
